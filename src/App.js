b68aff6fdae835e6e0efd7b5298a79d488dbdece
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-10-17 13:14:08 +0530

    create asser file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './Components/NavBar';
+import { HerrorBanner } from './Components/HerrorBanner';
 
 function App() {
   return (
     <div className="flex flex-col ">
       <NavBar />
-
+      <HerrorBanner />
     </div>
   );
 }
