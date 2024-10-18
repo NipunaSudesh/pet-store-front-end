4b35cca424d3c2a4072c942c479ec2a1c8218769
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-10-18 10:11:09 +0530

    created banner

--- src/Components/BannerTop.jsx
+++ src/Components/BannerTop.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import girl from '../assets/images/girlWithDog.png';
+import { FaPlay } from "react-icons/fa";
 
 export const BannerTop = () => {
   return (
-    <div className='w-[1180px] h-[378px] absolute top-[1681px] left-[130px] rounded-[20px] bg-[#003459]'>
+    <div className='w-[1180px] h-[378px] absolute top-[1681px] left-[130px] rounded-[20px] bg-[#003459] z-0 overflow-hidden'>
 
-<div className="w-[787.54px] h-[787.54px] absolute top-[28.72px] left-[41.49px] gap-0 rounded-[99px] opacity-100 rotate-[-28.25deg] bg-[#002A48] z-10"></div>
+<div className="relative w-[desired-width] h-[desired-height]">
+<div className="w-[787.54px] h-[787.54px] absolute top-[188.72px] left-[-155.49px] z-10 gap-0 rounded-[99px] opacity-100 -rotate-[-28.25deg] bg-[#002A48]"></div>
+<div className="w-[782.29px] h-[635px] absolute top-[-260px] left-[576.67px] gap-0 rounded-[99px]  opacity-100 -rotate-[-25.23deg] bg-[#FCEED5] z-10"></div>
+</div>
+<div className='absolute top-[36px] left-[20px] w-[513px] h-[342px]  z-30'>
+        <img src={girl} alt='A girl with a dog' className='w-full h-full object-cover' />
+      </div>
+
+
+<div>
     </div>
+    <div className="relative flex justify-end">
+    <h1 className='w-[535px] h-[68px] font-SVN-Gilroy top-[64px] absolute left-[692px] font-extrabold text-[52px] leading-[64px] mb-4 text-right z-30 text-[#003459] pr-[70px]'>
+        One more friend
+      </h1>
+      <h2 className='w-[484px] h-[60px] top-[134px] absolute left-[741px] font-SVN-Gilroy font-bold text-[36px] leading-[54px] mb-4 text-right z-30 text-[#003459] pr-[70px]'>
+        Thousands more fun!
+      </h2>
+      <p className='w-[425px] h-[54px] top-[196px] absolute left-[781px] font-SVN-Gilroy font-medium text-[12px] text-[#242B33] mb-6 text-right z-30 pr-[50px]'>
+        Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets that can meet your needs!
+      </p>
+      </div>
+      <button className="flex items-center border border-[#003459] text-[#003459] h-[48px] absolute w-[161px] top-[274px] left-[791px] px-[28px] py-[14px] rounded-full transition-all hover:bg-gray-100 z-30 ">
+        View Intro
+        <span className="ml-2 flex justify-center items-center w-[18px] h-[18px] rounded-full border border-[#003459]">
+          <FaPlay size={8} />
+        </span>
+      </button>
+      <button
+        className='bg-[#003459] w-[161px] text-white rounded-[57px] px-[28px] py-[14px] transition-all duration-300 hover:bg-[#0c1b26] h-[48px] absolute top-[274px] left-[1000px] z-30'
+      >
+        Explore Now
+      </button>
+          </div>
+    
+
   )
 }
--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,7 +1,9 @@
 import banner from './images/banner.png';
 import dog from './images/dog.png';
+import girl from './images/girlWithDog.png';
 
 export{
     banner ,
-    dog
+    dog,
+    girl
 }
