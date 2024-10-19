e08aee4ddf689975414f20f1709521038f050794
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-10-19 20:34:10 +0530

    Pet responced

--- src/Components/HomePage/Pet/Pet.jsx
+++ src/Components/HomePage/Pet/Pet.jsx
@@ -1,38 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoIosArrowForward } from "react-icons/io";
 import { PetCard } from './PetCard';
 
 export const Pet = () => {
+  const [pappies, setPappies] = useState([]);
+
+  useEffect(() => {
+      const fetchPappies = async () => {
+          try {
+              const response = await fetch('https://monitor-backend-rust.vercel.app/api/pets');
+              
+              if (!response.ok) {
+                  throw new Error('Network response was not ok');
+              }
+
+              const data = await response.json(); 
+              setPappies(data); 
+              console.log(data); 
+          } catch (error) {
+              console.error('Error fetching Pappies:', error);
+          }
+      };
+
+      fetchPappies(); 
+  }, []);
+  
   return (
-    <div className='relative w-[1440px] h-[986px] bg-white z-20 flex flex-col'>
+    <div className='relative w-full max-w-[1340px] h-auto bg-white z-20 flex flex-col px-4 md:px-10 '>
       
-      <div className='absolute flex w-full top-[60px] left-[130px]'>
-        <div className='w-[367px] h-[62px] gap-[2px]'>
-          <h2 className='w-[191px] h-[24px] font-SVN-Gilroy font-medium text-[16px] leading-[24px] text-[#000000]'>
+      <div className=' sm:ml-[130px] flex flex-col md:flex-row md:items-center w-full mt-10 justify-between'>
+        <div className='w-full md:w-auto'>
+          <h2 className='font-SVN-Gilroy font-medium text-[16px] leading-[24px] text-[#000000]'>
             What's new?
           </h2>
-          <h1 className='w-[367px] h-[36px] font-SVN-Gilroy font-bold text-[24px] leading-[36px] text-[#003459]'>
+          <h1 className='font-SVN-Gilroy font-bold text-[20px] md:text-[24px] leading-[36px] text-[#003459]'>
             Take a look at some of our pets
           </h1>
         </div>
+        
+        <div className='mt-4 md:mt-0 '>
+          <button className='flex items-center justify-between w-full md:w-[180px] h-[44px] rounded-[57px] border border-1.5 border-gray-500 px-[28px] py-[12px] gap-[8px] transition hover:bg-gray-100'>
+            View More
+            <IoIosArrowForward className='w-6 h-6 text-gray-700' />
+          </button>
+        </div>
       </div>
 
-      
-      <div className='absolute top-[78px] right-[130px]'>
-        <button className='flex items-center justify-between w-[180px] h-[44px] rounded-[57px] border border-1.5 border-gray-500 px-[28px] py-[12px] gap-[8px] transition hover:bg-gray-100'>
-          View More
-          <IoIosArrowForward className='w-6 h-6 text-gray-700' />
-        </button>
-      </div>
-      <div className='top-[158px] w-full absolute grid grid-cols-4 gap-[8px] px-[130px]'>
-      <PetCard />
-      <PetCard />
-      <PetCard />
-      <PetCard />
-      <PetCard />
-      <PetCard />
-      <PetCard />
-      <PetCard />
+      <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-10 sm:l-[130px] gap-4 w-full'>
+        {pappies.map((pappy, index) => (
+            <PetCard 
+                key={index}
+                id={pappy.id}
+                breed={pappy.breed}
+                gender={pappy.gender}
+                age={pappy.age}
+                price={pappy.price}
+                image={pappy.image}
+            />
+        ))}
       </div>
     </div>
   );
