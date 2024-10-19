fd338d70faecd20628c092271fc5ef7144e5d9f1
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-10-19 18:35:04 +0530

    petcard make responsive

--- src/Components/HomePage/Pet/PetCard.jsx
+++ src/Components/HomePage/Pet/PetCard.jsx
@@ -3,16 +3,16 @@ import { LuDot } from "react-icons/lu";
 
 export const PetCard = ({ id, breed, gender, age, price, image }) => {
   return (
-    <div className="w-[280px] mb-2 h-[378px] rounded-[12px] p-[8px] gap-[8px] bg-gray-200 transition-transform transform hover:scale-105 shadow-lg">
+    <div className="max-w-[280px] w-full mb-2 h-auto rounded-[12px] p-4 gap-2 bg-gray-200 transition-transform transform hover:scale-105 shadow-lg">
       <div className='flex flex-col'>
         <div>
-          <img src={image} alt={breed} className='w-[264px] h-[264px] rounded' />
+          <img src={image} alt={breed} className='w-full h-[200px] object-cover rounded md:h-[264px]' />
         </div>
         <div className='flex flex-col justify-start mt-4'>
           <h1 className="font-SVN-Gilroy font-bold text-[16px] leading-[24px] text-left text-[#00171F]">
             {id} - {breed}
           </h1>
-          <div className='flex gap-3'>
+          <div className='flex gap-2 flex-wrap'>
             <h2 className="font-SVN-Gilroy font-medium text-[12px] leading-[18px] text-left text-[#667479]">Gender: {gender}</h2>
             <LuDot />
             <h2 className="font-SVN-Gilroy font-medium text-[12px] leading-[18px] text-left text-[#667479]">Age: {age}</h2>
--- src/Components/ProductDetails/Pappies.jsx
+++ src/Components/ProductDetails/Pappies.jsx
@@ -35,7 +35,7 @@ export const Pappies = () => {
                 </h1>
             </div>
 
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10'>
+            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-10'>
                 {pappies.map((pappy, index) => (
                     <PetCard 
                         key={index}
